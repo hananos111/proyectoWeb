a03ef10d68a78a3859bde390d1a3b8ce3050b2b2
hananos111 <hananbcn111@gmail.com>
2026-09-08 10:42:36 +0200

    Add files via upload

---
+++ Coche.java
@@ -0,0 +1,22 @@
+public class Coche extends Vehiculo
+{
+    private int puertas;
+
+    public Coche(int p)
+    {
+         super(0); // Si se elimina da error de compilación
+         puertas = p;
+    }  
+
+    public static void main(String[] args) {
+        Vehiculo miVehiculo=new Vehiculo(0); // Moto
+        Coche miCoche=new Coche(4); // Coche
+        miVehiculo.setRuedas(2);
+        miCoche.setruedas(4/2);
+        
+        System.out.println(Vehiculo.TIPO); // Debe poderse imprimir por pantalla la propiedad TIPO desde este método
+        
+        miCoche.setPuertas(5); // Deben asignarse 5 puertas al coche usando el método "setPuertas()".
+        System.out.println(miCoche.getPuertas()); // Deben mostrarse las puertas del vehículo usando el método "getPuertas()"   
+    }
+}
